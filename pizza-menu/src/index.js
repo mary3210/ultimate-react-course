b37df68e63ec5c9d052961b14f86b815de69288b
Mary Rodriguez <maryrodri3210@gmail.com>
2024-10-11 17:00:51 -0500

    created footer and menu  component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -48,11 +48,32 @@ const pizzaData = [
 
 function App() {
     return <div>
-        <h1> Hello React!</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
+        <Header />
+        <Menu />
+        <Footer />
     </div>;
+}
+
+function Header() {
+    return <h1> Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+    return (
+        <div>
+            <h2>Our menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    return <footer> {new Date().toLocaleTimeString()}. We're currently open</footer>
+    // return React.createElement('footer', null, "we're currently open!");
 }
 
 function Pizza() {
