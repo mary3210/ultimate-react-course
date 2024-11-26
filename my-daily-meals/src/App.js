ea856903878df3b610b5d45244d939564f64f517
Mary Rodriguez <maryrodri3210@gmail.com>
2024-11-26 17:20:10 -0600

    created meal list component

--- my-daily-meals/src/App.js
+++ my-daily-meals/src/App.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
 
-
+const seedData = [
+  {
+    time: "Morning",
+    mealName: "Parfait",
+    ingredients: "Greek Yogurt, one apple, honey"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-
+      <MealList />
     </div>
   );
 }
+
+function MealList() {
+  console.log(seedData)
+  return (
+    <div>
+      <p> </p>
+      <p>{seedData[0].time}</p>
+    </div>
+
+  )
+}
+
+
 
 export default App;
