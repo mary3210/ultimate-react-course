3e3a434c6387e599f336b93f9b3e9ef452b193dd
Mary Rodriguez <maryrodri3210@gmail.com>
2024-12-02 18:42:43 -0600

    delete feature is now available

--- my-daily-meals/src/App.js
+++ my-daily-meals/src/App.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
+
 const seedData = [
   {
     time: "Morning",
     mealName: "Parfait",
-    ingredients: "Greek Yogurt, one apple, honey"
+    ingredients: "Greek Yogurt, one apple, honey",
+    calories: 300
   }
 ];
 
@@ -14,7 +16,18 @@ function App() {
 
   function handleAddMeal(meal) {
     setMeals((meals) => [...meals, meal])
-    console.log(meals)
+
+  }
+
+  function handleDeleteMeal(mealId) {
+    console.log(meals[mealId])
+    if (meals[mealId]) {
+
+      let potato = meals.toSpliced(mealId, 1)
+      console.log(potato)
+      setMeals((meals) => potato)
+    }
+
   }
 
   function handleShow() {
@@ -23,36 +36,51 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Meals Today</h1>
-      <MealList meals={meals} />
-      {show ?
-        <AddMeal handleAddMeal={handleAddMeal} handleShow={handleShow} /> : <button onClick={handleShow}>ADD MEAL</button>}
+      <div className="header">
+        <h1>My Meals Today</h1>
+      </div>
+      <div className="contents">
+        <div>
+          <MealList meals={meals} handleDeleteMeal={handleDeleteMeal} />
+        </div>
+        <div>
+          {show ?
+            <AddMeal handleAddMeal={handleAddMeal} handleShow={handleShow} /> : <button className="addMealBtn" onClick={handleShow}>ADD MEAL</button>}
+        </div>
+
+      </div>
+      <TotalCalories meals={meals} />
     </div>
 
   );
 }
 
-function MealList({ meals }) {
+function MealList({ meals, handleDeleteMeal }) {
+
 
   return (
-    <div>
-      <Meal meals={meals} />
+    <div >
+      <Meal meals={meals} handleDeleteMeal={handleDeleteMeal} />
 
     </div>
 
   )
 }
 
-function Meal({ meals }) {
-  console.log(meals)
+function Meal({ meals, handleDeleteMeal }) {
+
+
   return (
     <div>
-      {meals.map((meal) =>
-        <div>
-          <p>Time: {meal.time}</p>
-          <p>Name of Meal: {meal.mealName}</p>
-          <p>Ingredients: {meal.ingredients}</p>
 
+      {meals && meals.map((meal, idx) =>
+        <div className="indivMeal" key={idx}>
+
+          <p>This {meal.time},</p>
+          <p>I ate {meal.mealName}.</p>
+          <p>Ingredients: {meal.ingredients}</p>
+          <p>Calories: {meal.calories}</p>
+          <button onClick={() => handleDeleteMeal(idx)}>delete</button>
         </div>)}
     </div>
   )
@@ -62,6 +90,7 @@ function AddMeal({ handleAddMeal, handleShow }) {
   const [time, setTime] = useState("morning")
   const [mealName, setMealName] = useState("")
   const [ingredients, setIngredients] = useState("")
+  const [calories, setCalories] = useState("")
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -69,7 +98,8 @@ function AddMeal({ handleAddMeal, handleShow }) {
     const meal = {
       time,
       mealName,
-      ingredients
+      ingredients,
+      calories
     };
 
     handleAddMeal(meal)
@@ -93,9 +123,13 @@ function AddMeal({ handleAddMeal, handleShow }) {
 
   };
 
+  function handleCalories(e) {
+    setCalories(e.target.value)
+  }
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="mealForm">
         <div>
           <label>Meal Time</label>
           <select onChange={handleMealTime} value={time}>
@@ -113,11 +147,43 @@ function AddMeal({ handleAddMeal, handleShow }) {
           <label>Ingredients</label>
           <textarea size="300" type="text" value={ingredients} onChange={handleIngredients}></textarea>
         </div>
+        <div>
+          <label>Calories</label>
+          <input type="text" value={calories} onChange={handleCalories}></input>
+        </div>
         <button>Submit</button>
 
       </form>
       <button onClick={handleShow}>Cancel</button>
     </>
+  )
+}
+
+
+
+/// NEED TO FIX HERE 
+function TotalCalories({ meals }) {
+  const [calorieNum, setCalorieNum] = useState(0);
+  let temp = 0;
+  function calculate() {
+
+
+
+
+    meals.map((meal, idx) => temp += meal.calories)
+
+
+
+  }
+  calculate();
+  // console.log(temp)
+  // setCalorieNum((prevNum) => temp)
+
+  return (
+    <div>
+      <h2>Total amount of calories ate today: {calorieNum} </h2>
+
+    </div>
   )
 }
 
